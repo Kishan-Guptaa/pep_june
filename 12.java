ef2a5c3eb8bc23ca65b4cfa55b3ae1ac620c1f7c
Kishan-Guptaa <kishangupta.code@gmail.com>
2026-06-27 23:05:23 +0530

    add implementation of findLadders method to solve the word ladder II problem

---
+++ 12.java
@@ -0,0 +1,88 @@
+// https://leetcode.com/problems/word-ladder-ii/
+
+class Solution {
+
+    Map<String, List<String>> parent = new HashMap<>();
+    List<List<String>> ans = new ArrayList<>();
+    List<String> path = new ArrayList<>();
+
+    public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
+
+        Set<String> dict = new HashSet<>(wordList);
+
+        if (!dict.contains(endWord))
+            return ans;
+
+        Map<String, Integer> level = new HashMap<>();
+        Queue<String> q = new LinkedList<>();
+
+        q.offer(beginWord);
+        level.put(beginWord, 0);
+
+        while (!q.isEmpty()) {
+
+            int size = q.size();
+
+            for (int s = 0; s < size; s++) {
+
+                String word = q.poll();
+                int currLevel = level.get(word);
+
+                char[] arr = word.toCharArray();
+
+                for (int i = 0; i < arr.length; i++) {
+
+                    char old = arr[i];
+
+                    for (char ch = 'a'; ch <= 'z'; ch++) {
+
+                        arr[i] = ch;
+                        String next = new String(arr);
+
+                        if (!dict.contains(next))
+                            continue;
+
+                        if (!level.containsKey(next)) {
+                            level.put(next, currLevel + 1);
+                            q.offer(next);
+                            parent.putIfAbsent(next, new ArrayList<>());
+                            parent.get(next).add(word);
+                        }
+                        else if (level.get(next) == currLevel + 1) {
+                            parent.get(next).add(word);
+                        }
+                    }
+
+                    arr[i] = old;
+                }
+            }
+        }
+
+        if (!level.containsKey(endWord))
+            return ans;
+
+        path.add(endWord);
+        dfs(endWord, beginWord);
+
+        return ans;
+    }
+
+    private void dfs(String word, String beginWord) {
+
+        if (word.equals(beginWord)) {
+            List<String> temp = new ArrayList<>(path);
+            Collections.reverse(temp);
+            ans.add(temp);
+            return;
+        }
+
+        if (!parent.containsKey(word))
+            return;
+
+        for (String prev : parent.get(word)) {
+            path.add(prev);
+            dfs(prev, beginWord);
+            path.remove(path.size() - 1);
+        }
+    }
+}
