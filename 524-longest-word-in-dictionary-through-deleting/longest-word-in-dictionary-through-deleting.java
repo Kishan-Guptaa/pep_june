c6cd1d37151baf7b28b222d9350229a1fcfbe581
Kishan Gupta <166640168+Kishan-Guptaa@users.noreply.github.com>
2026-07-31 19:48:39 +0530

    Time: 17 ms (67.89%) | Memory: 47.5 MB (77.61%) - LeetSync

---
+++ 524-longest-word-in-dictionary-through-deleting/longest-word-in-dictionary-through-deleting.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String findLongestWord(String s, List<String> dictionary) {
+        String ans = "";
+        for(String word : dictionary){
+            if(subsequence(s, word)){
+                if(word.length() > ans.length() || word.length() == ans.length() && word.compareTo(ans) < 0){
+                    ans = word;
+                }
+            }
+        }
+        return ans;
+    }
+    private boolean subsequence(String s, String word){
+        int i = 0;
+        int j = 0;
+        while(i < s.length() && j < word.length()){
+            if(s.charAt(i) == word.charAt(j)){
+                j++;
+            }
+            i++;
+        }
+        return j == word.length();
+    }
+}
