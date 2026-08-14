bce20bbcd8a53397903deb31779769d4945f13c3
Kishan Gupta <166640168+Kishan-Guptaa@users.noreply.github.com>
2026-08-14 16:18:34 +0530

    Time: 1 ms (81.77%) | Memory: 42.8 MB (79.12%) - LeetSync

---
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        return build(s).equals(build(t));
+    }
+    private String build(String s){
+        StringBuilder sb = new StringBuilder();
+        for(char ch : s.toCharArray()){
+            if( ch == '#'){
+                if(sb.length() > 0){
+                    sb.deleteCharAt(sb.length() - 1);
+                }
+            }
+            else{
+                sb.append(ch);
+            }
+        }
+        return sb.toString();
+    }
+}
