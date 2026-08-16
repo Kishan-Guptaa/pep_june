4b810663f4a1c1a095841ce24a106a2acdaa4f24
Kishan Gupta <166640168+Kishan-Guptaa@users.noreply.github.com>
2026-08-16 22:58:57 +0530

    Time: 533 ms (94.37%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2110-employees-with-missing-information/employees-with-missing-information.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT employee_id from Employees where employee_id not in (Select employee_id from Salaries)
+union
+Select employee_id from Salaries where employee_id not in (Select employee_id from Employees)
+
+
+order by employee_id asc
