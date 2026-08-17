4c5314f5c40659931909ef2d3a25bf9e2f7fffdb
Kishan Gupta <166640168+Kishan-Guptaa@users.noreply.github.com>
2026-08-17 12:07:27 +0530

    Time: 133 ms (35.03%) | Memory: 123.2 MB (86.29%) - LeetSync

---
+++ 4000-count-bowl-subarrays/count-bowl-subarrays.java
@@ -0,0 +1,29 @@
+class Solution {
+    public long bowlSubarrays(int[] nums) {
+        Stack<Integer>st = new Stack<>();
+        int ans = 0;
+        for(int i=0; i<nums.length; i++){
+            while(!st.isEmpty() && nums[st.peek()] < nums[i]){
+                int j = st.pop();
+                if(i - j >= 2){
+                    ans++;
+                }
+            }
+            st.push(i);
+
+        }
+
+        st.clear();
+        
+        for(int i=nums.length-1; i>=0; i--){
+            while(!st.isEmpty() && nums[st.peek()] < nums[i]){
+                int j = st.pop();
+                if( j-i >= 2){
+                    ans++;
+                }
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+}
