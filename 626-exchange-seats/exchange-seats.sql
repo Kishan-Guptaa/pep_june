9bd6e98bdffd0ff1b9cb62c97b4949e9a146e0cb
Kishan Gupta <166640168+Kishan-Guptaa@users.noreply.github.com>
2026-08-25 21:59:11 +0530

    Time: 526 ms (9.26%) | Memory: 0B (100.00%) - LeetSync

---
+++ 626-exchange-seats/exchange-seats.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+Select case when id % 2 = 1 and id < (Select max(id) from Seat) then id + 1 when id % 2 = 0 then id - 1 else id end as id, student from Seat order by id asc
