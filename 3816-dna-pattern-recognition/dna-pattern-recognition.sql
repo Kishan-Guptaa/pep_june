d0234f9ace2fb2baa8fe3b74e062102967febdc9
Kishan Gupta <166640168+Kishan-Guptaa@users.noreply.github.com>
2026-08-27 23:50:18 +0530

    Time: 385 ms (16.07%) | Memory: 0B (100.00%) - LeetSync

---
+++ 3816-dna-pattern-recognition/dna-pattern-recognition.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select sample_id, dna_sequence, species, (dna_sequence LIKE 'ATG%') as has_start, (dna_sequence like '%TAA' or dna_sequence like '%TAG' or dna_sequence like '%TGA') as has_stop, (dna_sequence like '%ATAT%') as has_atat, (dna_sequence like '%GGG%') as has_ggg from Samples
