a2dc7da0b84fd45e8233b64df0a93240b19339ea
Kishan Gupta <166640168+Kishan-Guptaa@users.noreply.github.com>
2026-08-31 19:28:24 +0530

    Time: 37 ms (76.08%) | Memory: 98.9 MB (19.47%) - LeetSync

--- 456-132-pattern/132-pattern.java
+++ 456-132-pattern/132-pattern.java
@@ -1,14 +1,13 @@
 class Solution {
     public boolean find132pattern(int[] nums) {
         Stack<Integer>st = new Stack<>();
-        int thirdNumber = Integer.MIN_VALUE;
-        for(int i=nums.length - 1; i>=0; i--){
-            if(nums[i] < thirdNumber){
+        int ThirdNumber = Integer.MIN_VALUE;
+        for(int i = nums.length - 1; i >= 0; i--){
+            if(nums[i] < ThirdNumber){
                 return true;
             }
-
             while(!st.isEmpty() && nums[i] > st.peek()){
-                thirdNumber = st.pop();
+                ThirdNumber = st.pop();
             }
             st.push(nums[i]);
         }
