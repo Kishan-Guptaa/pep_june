0fbe7d76ac0572ec2bfa6efa30e02b3988b71c19
Kishan Gupta <166640168+Kishan-Guptaa@users.noreply.github.com>
2026-09-13 19:30:51 +0530

    Time: 10 ms (12.50%) | Memory: 47.2 MB (33.26%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,48 +1,45 @@
 class Solution {
-    private int[]  topoSort(int n, List<Integer>[] adj ){
-        int[] indegree = new int[n];
-        for(int i=0; i<n; i++){
-            for(int it : adj[i]){
+    private int[] topoSort(int V, List<List<Integer>>adj){
+        int[] indegree = new int[V];
+        for(int i=0; i<V; i++){
+            for(int it : adj.get(i)){
                 indegree[it]++;
             }
         }
-
         Queue<Integer>q = new LinkedList<>();
-        for(int i=0; i<n; i++){
+        for(int i=0; i<V; i++){
             if(indegree[i] == 0){
                 q.add(i);
             }
         }
-        int[] ans = new int[n];
-        int idx = 0;
+
+        int[] topo = new int[V];
+        int index = 0;
         while(!q.isEmpty()){
-            int node = q.remove();
-            ans[idx++] = node;
-            for(int it : adj[node]){
-                indegree[it]--;
-                if(indegree[it] == 0){
-                    q.add(it);
+            int node = q.poll();
+            topo[index++] = node;
+            for(int neighbor : adj.get(node)){
+                indegree[neighbor]--;
+                if(indegree[neighbor] == 0){
+                    q.add(neighbor);
                 }
             }
-            
         }
-
-        return Arrays.copyOfRange(ans, 0, idx);
+        return Arrays.copyOfRange(topo, 0, index);
     }
-    public boolean canFinish(int n, int[][] prerequisites) {
-        List<Integer>[] adj = new ArrayList[n];
-        for(int i=0; i<n; i++){
-            adj[i] = new ArrayList<>();
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>>adj = new ArrayList<>();
+        // int n = prerequisites.length;
+        for(int i=0; i<numCourses; i++){
+            adj.add(new ArrayList<>());
         }
-
-        for(int[] it : prerequisites){
-            int u = it[0];
-            int v = it[1];
-            adj[u].add(v);
+        for(int[] pre : prerequisites){
+            int u = pre[0];
+            int v = pre[1];
+            adj.get(u).add(v);
         }
-
-        int[] topo = topoSort(n, adj);
-        if(topo.length < n){
+        int[] topo = topoSort(numCourses, adj);
+        if(topo.length < numCourses){
             return false;
         }
         return true;
