4d0a55a0c5139184232a029b6856f28ccb2c5c08
Kishan Gupta <166640168+Kishan-Guptaa@users.noreply.github.com>
2026-09-13 21:04:14 +0530

    Time: 7 ms (29.29%) | Memory: 46.9 MB (39.15%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,47 +1,48 @@
 class Solution {
-    private int[] topoSort(int n, List<Integer>[]adj){
-        int[] indegree = new int[n];
-        for(int i=0; i<n; i++){
-            for(int it : adj[i]){
+    private int[] topoSort(int V, List<List<Integer>>adj){
+        int[] indegree = new int[V];
+        for(int i=0; i<V; i++){
+            for(int it : adj.get(i)){
                 indegree[it]++;
             }
         }
-
         Queue<Integer>q = new LinkedList<>();
-        for(int i=0; i<n; i++){
+        for(int i=0; i<V; i++){
             if(indegree[i] == 0){
                 q.add(i);
             }
         }
-        int[] ans =  new int[n];
-        int idx = 0;
+        int[] ans = new int[V];
+        int index = 0;
         while(!q.isEmpty()){
             int node = q.remove();
-            ans[idx++] = node;
-            for(int it : adj[node]){
-                indegree[it]--;
-                if(indegree[it] == 0){
-                    q.add(it);
+            ans[index++] = node;
+            for(int neighbor : adj.get(node)){
+                indegree[neighbor]--;
+                if(indegree[neighbor] == 0){
+                    q.add(neighbor);
                 }
             }
         }
-
-        if(idx != n){
+        if(index != V){
             return new int[0];
         }
         return ans;
     }
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        List<Integer>[] adj = new ArrayList[numCourses];
+        int n = prerequisites.length;
+        List<List<Integer>>adj = new ArrayList<>();
         for(int i=0; i<numCourses; i++){
-            adj[i] = new ArrayList<>();
+            adj.add(new ArrayList<>());
+        }
+        for(int[] pre : prerequisites){
+            int u = pre[0];
+            int v = pre[1];
+
+            adj.get(v).add(u);
         }
 
-        for(int[] it : prerequisites){
-            int u = it[0];
-            int v = it[1];
-            adj[v].add(u);
-        }
-        return topoSort(numCourses, adj);
+        int[] topo = topoSort(numCourses, adj);
+        return topo;
     }
 }
