ca078a2754490c104ced73dab48b9304547c922d
Kishan Gupta <166640168+Kishan-Guptaa@users.noreply.github.com>
2026-09-23 16:27:31 +0530

    Time: 3 ms (63.21%) | Memory: 46.5 MB (66.77%) - LeetSync

--- 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.java
+++ 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.java
@@ -13,14 +13,12 @@ class Solution {
             arr[i][0] = count;
             arr[i][1] = i;
         }
-
         Arrays.sort(arr, (a,b) -> {
             if(a[0] == b[0]){
                 return a[1] - b[1];
             }
             return a[0] - b[0];
         });
-
         int[] ans = new int[k];
         for(int i=0; i<k; i++){
             ans[i] = arr[i][1];
