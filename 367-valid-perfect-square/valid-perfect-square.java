02c2b28177ce4df81a4894cd0e525be3a4dcae05
Kishan Gupta <166640168+Kishan-Guptaa@users.noreply.github.com>
2026-09-23 16:32:51 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (37.63%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        if(num<2){
+            return true;
+        }
+        long left = 2;
+        long right = num/2;
+        while(left <= right){
+            long mid = (left + right) / 2;
+            long sq = mid * mid;
+            if(sq == num){
+                return true;
+            }
+            else if(sq > num){
+                right = mid - 1;
+            }
+            else{
+                left = mid + 1;
+            }
+        }
+        return false;
+    }
+}
