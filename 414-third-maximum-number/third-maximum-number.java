69e52cd113dcc0119eb04aa7d603b294280292b5
Kishan Gupta <166640168+Kishan-Guptaa@users.noreply.github.com>
2026-09-25 23:16:18 +0530

    Time: 1 ms (98.81%) | Memory: 44.5 MB (93.72%) - LeetSync

---
+++ 414-third-maximum-number/third-maximum-number.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int thirdMax(int[] nums) {
+        long n1 = Long.MIN_VALUE;
+        long n2 = Long.MIN_VALUE;
+        long n3 = Long.MIN_VALUE;
+        for(int i=0; i<nums.length; i++){
+            if(n1 == nums[i] || n2 == nums[i] || n3 == nums[i]){
+                continue;
+            }
+            if(nums[i] > n1){
+                n3 = n2;
+                n2 = n1;
+                n1 = nums[i];
+            }
+            else if(nums[i] > n2){
+                n3 = n2;
+                n2 = nums[i];
+            }
+            else if(nums[i] > n3){
+                n3 = nums[i];
+            }
+        }
+         return n3 == Long.MIN_VALUE ? (int) n1 : (int) n3;
+    }
+}
